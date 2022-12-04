25bdb1a6d2cc6baba5c5121cd01ea7dee6fe75a6
YevheniyZavada <zavada1997@gmail.com>
2022-12-04 20:24:53 +0200

    the first commit

---
+++ src/ShapeTest.java
@@ -0,0 +1,17 @@
+public class ShapeTest {
+    public static void main(String[] args) {
+        Shape shape = new Circle();
+        Shape shape1 = new Quad();
+        Shape shape2 = new Rectangle();
+        Shape shape3 = new Triangle();
+        Shape shape4 = new Rhombus();
+
+        System.out.println(shape.getName());
+        System.out.println(shape1.getName());
+        System.out.println(shape2.getName());
+        System.out.println(shape3.getName());
+        System.out.println(shape4.getName());
+
+
+    }
+}
